caf972193d961f7138f6f012bf948536873b4e1b
Donnie Goodson <donnie.goodson@microsoft.com>
2020-03-06 13:11:32 -0800

    Test sort order of packageIds in Update tab

--- src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/PackageFeeds/UpdatePackageFeed.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/PackageFeeds/UpdatePackageFeed.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -102,8 +102,7 @@ namespace NuGet.PackageManagement.VisualStudio
             var packages = _installedPackages
                 .Where(p => !p.IsAutoReferenced())
                 .GetEarliest()
-                .Where(p => p.Id.IndexOf(searchText, StringComparison.OrdinalIgnoreCase) != -1)
-                .OrderBy(p => p.Id);
+                .Where(p => p.Id.IndexOf(searchText, StringComparison.OrdinalIgnoreCase) != -1);
 
             // Prefetch metadata for all installed packages
             var prefetch = await TaskCombinators.ThrottledAsync(
@@ -164,7 +163,7 @@ namespace NuGet.PackageManagement.VisualStudio
                     p => p.Identity,
                     (id, pl) => pl.First());
 
-            return uniquePackages.ToArray();
+            return uniquePackages.OrderBy(p => p.Identity.Id).ToArray();
         }
 
         private static IEnumerable<VersionInfo> ToVersionInfo(IEnumerable<IPackageSearchMetadata> packages)
